845effd485cb8ba66804f635af5d4a3f072e663f
wterkin <appmailbox@mail.ru>
2020-04-18 18:37:47 +0300

    Начало работы

--- clock.py
+++ clock.py
@@ -1,1 +1,13 @@
-# 
+#!/bin/python3
+
+# *** Константы конфига
+LATCH = "latch"
+CLOCK = "clock"
+SERIAL = "serial"
+
+class CPerfectClock():
+    """Основной класс."""
+    def __init(pdc_сonfig):
+        """Конструктор."""
+        self.config = pdc_сonfig
+        pass
--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-# 
+#!/bin/python3
+"""Стартовый модуль."""
+ import clock
+ 
+# *** Конфигурация
+CONFIG = {clock.LATCH: 7,
+          clock.CLOCK: 8,
+          clock.SERIAL: 6}
+
+
+if __name__ == '__main__':
+    app = CPerfectClock(CONFIG)
+    app.run()
