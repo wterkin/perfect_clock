8455be116b13500ffad1ccd28aa3c80b0645c761
Andrey Pakhomenkov <pakhomenkov.ap@gmail.com>
2021-04-28 09:28:21 +0300

    Адресация у этого дисплея дурнаааааая...

--- main.py
+++ main.py
@@ -1,31 +1,63 @@
 #!/bin/python3
 """Стартовый модуль."""
-import clock
+#!import clock
  
+from machine import Pin, Timer, RTC
+import tm1637
 
-def estabilish_connection():
-    """ Процедура осуществляет соединение с выбранной сетью Wi-Fi """
+SEG_A = 0b00000001
+SEG_B = 0b00000010
+SEG_C = 0b00000100
+SEG_D = 0b00001000
+SEG_E = 0b00010000
+SEG_F = 0b00100000
+SEG_G = 0b01000000
 
-    wlan = network.WLAN(network.STA_IF)
-    wlan.active(True)
+#          11->A
+#        ---------
+# 10->F |   5->G  | 7->B
+#        ---------  
+#  1->E |         | 4->C
+#        --------- 
+#          2->D
 
-    if not wlan.isconnected():
+DIGITS_SET = [0b00000110, 0b01011011, 0b01001111, 0b01100110, 0b01101101, 0b01111101 ]
 
-        wlan.connect(SSID, PASSWORD)
-        while not wlan.isconnected():
+#HELLO2 = [0b01110110, 0b00111000, 0b00111000, 0b0011111, 0, 0]
+#HELLO3 = [0b00111000, 0b00111000, 0b01110110, 0, 0, 0b0011111]
+#HELLO4 = [0b00111000, 0b01111001, ]
+# 321654
+H = 0b01110110
+E = 0b01111001
+L = 0b00111000
+O = 0b00111111
 
-            pass
-    g_conn_led.on()
+HELLO = [L, E, H, 0, O, L]
 
+DIGIT1 = 3
+DIGIT2 = 2
+
+
+def test():
+    
+    clock_pin = Pin(0)
+    dio_pin = Pin(4)
+    tm = tm1637.TM1637(clk=clock_pin, dio=dio_pin)
+    tm.write([0b00000000, 0b00000000, 0b00000000, 0b00000000, 0b00000000, 0b00000000])
+    tm.write(HELLO)
+    #tm.write([0b00000000, 0b00000000, 0b00000000, 0b00000000, 0b00000000, 0b00000000])
+    
 
 def main():
 
-    app = clock.CPerfectClock()
-    app.run()
-    if clock.DEBUG:
-        
-        print("Terminated.")
 
+    test()
+    #!app = clock.CPerfectClock()
+    #!app.run()
+    #!if clock.DEBUG:
+        
+    #!    print("Terminated.")
+    
 
 # *** Если это главный модуль и отладка выключена..
 if (__name__ == '__main__'): # and not clock.DEBUG:
